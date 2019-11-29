c13cad48f9c3590107effe788045d6189dcb9021
lipo <rsq@kaishiba.com>
2019-11-29 16:21:00 +0800

    cache

--- src/main/java/org/study/redis/service/UserService.java
+++ src/main/java/org/study/redis/service/UserService.java
@@ -1,9 +1,15 @@
 package org.study.redis.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.study.redis.model.User;
 
+@Slf4j
 @Service
 public class UserService {
+    private static final String USER_CACHE = "USER_CACHE";
+
     public void update() {
         try {
             Thread.sleep(1000);
@@ -11,4 +17,13 @@ public class UserService {
             e.printStackTrace();
         }
     }
+
+    @Cacheable(value = USER_CACHE, key = "#id")
+    public User get(Integer id) {
+        User user = new User(1, "lipo");
+        log.info("select user = {}", user);
+        return user;
+    }
+
+
 }
